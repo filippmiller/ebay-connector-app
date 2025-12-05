8931ffa7509a716376295ba4b5589706a8732efb
YOUR NAME <filippmiller@gmail.com>
2025-12-05 20:23:34 +0300

    Fix AG Grid CSS conflict causing invisible grids

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'ag-grid-community/styles/ag-grid.css'
+
 import App from './App.tsx'
 import { UITweakProvider } from './contexts/UITweakContext'
 
